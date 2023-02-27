acfdb5027b2b96bfc9153d71343f76b02af45deb
xinjie <xinjie6016@gmail.com>
2023-02-27 17:45:46 +0800

    update

--- Weather/app.js
+++ Weather/app.js
@@ -33,9 +33,9 @@ form.addEventListener("submit", e => {
     });
 
     if (filteredArray.length > 0) {
-      msg.textContent = `You already know the weather for ${
+      msg.textContent = `You have known the weather for ${
         filteredArray[0].querySelector(".city-name span").textContent
-      } ...otherwise be more specific by providing the country code as well `;
+      } `;
       form.reset();
       input.focus();
       return;
@@ -73,7 +73,7 @@ form.addEventListener("submit", e => {
       list.appendChild(li);
     })
     .catch(() => {
-      msg.textContent = "Please search for a valid city ";
+      msg.textContent = "Please enter a valid city ";
     });
     
   msg.textContent = "";
