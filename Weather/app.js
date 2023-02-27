e0ae7cd3d55af6fe396d3efcb009dc9bee654536
xinjie <xinjie6016@gmail.com>
2023-02-27 17:40:29 +0800

    update

--- Weather/app.js
+++ Weather/app.js
@@ -1,7 +1,7 @@
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
-const list = document.querySelector(".ajax-section .cities");
+const list = document.querySelector(".section .cities");
 const apiKey = "b96a7f5f8b10075923fb2eafc78fe2f8";
 
 form.addEventListener("submit", e => {
@@ -9,15 +9,15 @@ form.addEventListener("submit", e => {
   let inputVal = input.value;
 
   //check if there's already a city
-  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItems = list.querySelectorAll(".section .city");
   const listItemsArray = Array.from(listItems);
 
   if (listItemsArray.length > 0) {
     const filteredArray = listItemsArray.filter(el => {
       let content = "";
-      //athens,gr
+      //selangor,my
       if (inputVal.includes(",")) {
-        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+        //selangor,myyyy->invalid country code, so we keep only the first part of inputVal
         if (inputVal.split(",")[1].length > 2) {
           inputVal = inputVal.split(",")[0];
           content = el
@@ -27,7 +27,6 @@ form.addEventListener("submit", e => {
           content = el.querySelector(".city-name").dataset.name.toLowerCase();
         }
       } else {
-        //athens
         content = el.querySelector(".city-name span").textContent.toLowerCase();
       }
       return content == inputVal.toLowerCase();
@@ -43,7 +42,6 @@ form.addEventListener("submit", e => {
     }
   }
 
-  //ajax here
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
   fetch(url)
